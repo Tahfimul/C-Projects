f517db6f87a2bf5902ebcd57b9d2c7705c797a55
Tahfimul <victory.josh.josh24@gmail.com>
2018-04-21 23:42:54 -0400

    March 24, 2018 Programs

---
+++ Program25.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+//Tahfimul Latif
+//Program 25
+//is Program 22 using for loop
+//March 24, 2018
+int main()
+{
+	int i;
+	for(i = 0; i<=5; i++)
+		cout<<"Christina\n";
+
+}
