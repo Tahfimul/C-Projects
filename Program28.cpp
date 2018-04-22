8419b87dc4719c1872e49b29a97e653a6046082f
Tahfimul <victory.josh.josh24@gmail.com>
2018-04-22 00:08:41 -0400

    March 24, 2018 Projects

---
+++ Program28.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+//Tahfimul Latif
+//Program28
+//to print odd number from 19 to -5
+//March 24, 2018
+int main()
+{
+	int w = 19;
+	while(w>=-5)
+	{
+		if(w%2 != 0)
+			cout<<w<<endl;
+		w--;
+	}
+}
