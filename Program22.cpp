4ec103dfc9d953e8124a750c3159bc4bb640fab4
Tahfimul <victory.josh.josh24@gmail.com>
2018-04-21 23:34:23 -0400

    March 24, 2018 Programs

---
+++ Program22.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+//Tahfimul Latif
+//Program 22
+//is Program 21 using while loop
+//March 24, 2018
+int main()
+{
+ int w = 0;
+ while(w<=5)
+ {
+   cout<<"Christina\n";
+   w++;
+ }
+}
