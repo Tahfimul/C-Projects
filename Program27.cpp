842d9b89fd8aeef9441f0b393b3599cdd6e71a05
Tahfimul <victory.josh.josh24@gmail.com>
2018-04-22 00:07:23 -0400

    March 24, 2018 Projects

---
+++ Program27.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+//Tahfimul Latif
+//Program27
+//to print even #s from -5 to 19
+//March 24, 2018
+int main()
+{
+	int w = -5;
+	while(w<=19)
+	{
+		cout<<w<<endl;
+		w++;
+	}
+}
