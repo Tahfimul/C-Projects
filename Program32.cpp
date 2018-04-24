0d30a1eb382ca32a25d0409d4a8b102dca25b9e1
Tahfimul <victory.josh.josh24@gmail.com>
2018-04-24 14:38:08 -0400

    March 24, 2018 Programs

---
+++ Program32.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+//Program 32 To print the sum of multiples of 5 between 1 and 21
+
+int main() {
+  int w=1, sum=0;
+  while (w<=21) {
+    if (w%5 == 0)
+      sum+=w;
+    w++;
+  }
+  cout<<"The sum is: "<<sum<<endl;
+  return 0;
+}
