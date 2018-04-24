ec18838c05f8a1b2a7c010fccefe0b54bea57012
Tahfimul <victory.josh.josh24@gmail.com>
2018-04-24 14:52:46 -0400

    March 24, 2018 Programs

---
+++ Program39.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+//Tahfimul Latif
+//Program39
+//to print the negative numbers of a list of 5 numbers: 3,2,-7,19,-18 (-19 to -2)
+//March 24, 2018
+int main()
+{
+  int w=-19;
+  while(w<=-2)
+  {
+    cout<<w<<endl;
+    w+=4;
+  }
+  return 0;
+}
