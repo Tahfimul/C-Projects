b4ae31e332a10a2e7c07b0dcd9ce6c66cbd1bf0b
Tahfimul <victory.josh.josh24@gmail.com>
2018-04-24 14:44:46 -0400

    March 24, 2018 Programs

---
+++ Program36.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+//Tahfimul Latif
+//Program36
+//To print a list of 5 numbers: 3, 2, -7, 19, -18
+//March 24, 2018
+int main()
+{
+  int w=1, num[5];
+  cout<<"Please input a list of numbers:";
+  while (w<=5) {
+  	cin>>num[w];
+    w++;
+  }
+  return 0;
+}
