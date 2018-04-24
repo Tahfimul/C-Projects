a3917fde8300fadf2e4b86e5813b9de81a730f00
Tahfimul <victory.josh.josh24@gmail.com>
2018-04-24 14:41:19 -0400

    March 24, 2018 Programs

---
+++ Program34.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+//Tahfimul Latif
+//Program34
+//to print every other number between 1 and 10 starting with 2
+//March 24, 2018
+int main() {
+  int w = 2;
+  while (w<=10) {
+    cout<<w<<endl;
+    w+=2;
+  }
+  return 0;
+}
