4c326b0f060c213565df2e5798437ff0c9da6147
Tahfimul <victory.josh.josh24@gmail.com>
2018-04-24 15:00:11 -0400

    March 24, 2018 Programs

---
+++ Program45.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+//Tahfimul Latif
+//Program45
+//to print odd number from 19 to -5 using FOR LOOP
+//March 24, 2018
+int main()
+{
+	int w = 19;
+  for (w; w >=-5; w--) {
+    if(w%2 != 0)
+			cout<<w<<endl;
+  }
+}
